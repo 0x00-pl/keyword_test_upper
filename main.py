9aa847bc5fe2fc6ae80a1943cbcba7ffff9a1e33
0x00-pl <0x00.pl@gmail.com>
2019-01-31 09:44:43 +0000

    add code

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ def msg_from_device(ser=None):
     if ser:
         res = ser.readline().decode('utf8')
         print('<<', res)
-        return res
+        if 'VAD' in res or res=='':
+            return res
+        else:
+            return msg_from_device(ser)
     else:
         return input()
 
@@ -34,7 +37,7 @@ def test_score(sound_path1, sound_path2, ser=None):
     return msg_from_device(ser)
 
 
-def test_all(filepath_list, ser=None):
+def test_all(filepath_list, ser=None, cb=None):
     ret = []
     length = len(filepath_list)
     total_test = length * (length - 1)
@@ -42,7 +45,9 @@ def test_all(filepath_list, ser=None):
         for j in range(i, length):
             res = test_score(filepath_list[i], filepath_list[j], ser)
             ret.append(res)
-            print('test {}/{}'.format(len(ret), total_test))
+            if cb:
+                cb(res)
+            print('[test] {}/{}'.format(len(ret), total_test))
 
     return ret
 
@@ -56,11 +61,26 @@ def read_all(root_path='kanzhitongxue'):
     return ret
 
 
+def dump_to_file(line):
+    with open('dump.txt', 'a') as df:
+        df.write(line)
+
+
+def list_ports():
+    import serial.tools.list_ports
+
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        if 'MyCDCDevice' in p.description:
+            print(p)
+
+
 def main():
+    dump_to_file('=======================================')
+    list_ports()
     filepath_list = read_all('kanzhitongxue/neg')
-    # with serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=5) as ser:
-    ser = None
-    res = test_all(filepath_list, ser)
+    with serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=5) as ser:
+        res = test_all(filepath_list, ser, cb=dump_to_file)
     np.save('output.npy', res)
 
 
